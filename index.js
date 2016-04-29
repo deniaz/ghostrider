b565d1da56bc429400ffdced715d7285abab630f
Robert Vogt <robert.vogt@namics.com>
2016-04-29 14:07:09 +0200

    Dynamically mock auth services

---
+++ index.js
@@ -0,0 +1,77 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const app = express();
+
+const apiaryBaseUrl = 'http://private-dcead-traildevils.apiary-mock.com';
+
+let users = require('./collections/users');
+
+app.use(bodyParser.json());
+
+app.post('/login', (req, res) => {
+	const login = req.body;
+
+	let user = users.find(user => {
+		return (user.username === login.username || user.email === login.email) && user.password === login.password;
+	});
+
+	if (user) {
+		res
+			.type('application/json')
+			.status(200)
+			.json({
+				auth_token: user.auth_token,
+			});
+	} else {
+		res
+			.status(404)
+			.send('Check your credentials.');
+	}
+});
+
+app.post('/register', (req, res) => {
+	const register = req.body;
+
+	let errors = [];
+	if (!register.email) {
+		errors.push('email');
+	}
+
+	if (!register.username) {
+		errors.push('username');
+	}
+
+	if (!register.password) {
+		errors.push('password');
+	}
+
+	if (!register.terms) {
+		errors.push('terms');
+	}
+
+	if (errors.length > 0) {
+		res
+			.status(400)
+			.send('Please fill out the following fields: ' + errors.join(', '));
+	} else {
+		const user = {
+			email: register.email,
+			username: register.username,
+			password: register.password,
+			auth_token: Math.random().toString(36).substring(7),
+		};
+
+		users.push(user);
+
+		res
+			.type('application/json')
+			.status(200)
+			.json({
+				auth_token: user.auth_token,
+			});
+	}
+});
+
+app.listen(1337, () => {
+	console.log("Let's Ride!");
+});
