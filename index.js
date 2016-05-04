6a4faa3704d38513f885f511cb57b93df3c6771a
Robert Vogt <robert.vogt@namics.com>
2016-05-04 20:29:25 +0200

    Add Estimations

--- index.js
+++ index.js
@@ -56,23 +56,23 @@ app.get('/trails', (req, res) => {
       image: null,
       headerImage: null,
       latLng: { lat: 47.5966, lng: 11.049 },
-      ratingValue: 0,
-      conditionEstimation: 0
+      ratingValue: 4,
+      conditionEstimation: 3
     }, {
       id: "2392e4dda4c9cffa041c08d22ef1641b",
       name: "Nordkette Singletrail Innsbruck",
       image: null,
       headerImage: null,
       latLng: { lat: 47.2606, lng: 11.3928 },
-      ratingValue: 0,
-      conditionEstimation: 0
+      ratingValue: 3.5,
+      conditionEstimation: 1
     }, {
       id: "7e821eee4b2fccf19d0708d22ef16426",
       name: "Dättnau",
       image: "http://traildevils.ch/img/vga/3947463b3a85c2bb00e708d22f015658.jpg",
       headerImage: null,
       latLng: { lat: 47.4767, lng: 8.69793 },
-      ratingValue: 0,
+      ratingValue: 2,
       conditionEstimation: 0
     }, {
       id: "de1ef457105ec33a580908d22ef1642b",
@@ -80,15 +80,15 @@ app.get('/trails', (req, res) => {
       image: "http://traildevils.ch/img/vga/432bacbbc41ac65aea6408d22f01740d.jpg",
       headerImage: null,
       latLng: { lat: 47.6672, lng: 8.38858 },
-      ratingValue: 0,
-      conditionEstimation: 0
+      ratingValue: 4.5,
+      conditionEstimation: 2
     }, {
       id: "37c2921b6473c380655408d22ef1642e",
       name: "NT Kronenwiese",
       image: "http://traildevils.ch/img/vga/52c57ead82edcc7447c908d22f0193be.jpg",
       headerImage: null,
       latLng: { lat: 47.3872, lng: 8.53584 },
-      ratingValue: 0,
+      ratingValue: 1.5,
       conditionEstimation: 0
     }, {
       id: "99367a3525ccc2520ee208d22ef16432",
@@ -96,15 +96,15 @@ app.get('/trails', (req, res) => {
       image: null,
       headerImage: null,
       latLng: { lat: 46.3327, lng: 14.309 },
-      ratingValue: 0,
-      conditionEstimation: 0
+      ratingValue: 2,
+      conditionEstimation: 4
     }, {
       id: "bfc313708f95c81e9eab08d22ef16438",
       name: "Flowpark Gößweinstein",
       image: "http://traildevils.ch/img/vga/3c45187105efc22d5cdb08d22f018bc2.jpg",
       headerImage: null,
       latLng: { lat: 49.7655, lng: 11.3446 },
-      ratingValue: 0,
+      ratingValue: 2.5,
       conditionEstimation: 0
     }]);
 });
